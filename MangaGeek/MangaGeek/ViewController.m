5af0e5ec2e86b5a24047a4fc09fc656b975c3eb2
MinhMera <nhatminhn900@gmail.com>
2015-07-19 11:05:04 +0700

    first test

--- MangaGeek/MangaGeek/ViewController.m
+++ MangaGeek/MangaGeek/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"viewDidload............");
+    NSLog(@"============= TEST GITHUB ===============");
     
      //[self parseJSONFile];
     // Do any additional setup after loading the view, typically from a nib.
